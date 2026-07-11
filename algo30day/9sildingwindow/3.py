9e2ff5f3b207c4067a574c6272513ad74278616b
Behruz <behruzqalqonov@gmail.com>
2026-07-12 00:20:02 +0500

    oxirgi yechimni qushdim

--- algo30day/9sildingwindow/3.py
+++ algo30day/9sildingwindow/3.py
@@ -5,7 +5,14 @@ def longest_unique(lst):
     max_len = 0
     while R < len(lst):
         if lst[R] not in window:
+            window.add(lst[R])
+            R +=1
+            max_len = max(max_len, R - L)
+        else:
+            window.remove(lst[L])
+            L +=1
+    return max_len
 
-    return len(lines)
 
-print(longest_unique([1,2,3,3,3,3]))
+
+print(longest_unique([1,2,3,2,4]))
