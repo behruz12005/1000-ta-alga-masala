24c372d6632cce4e1a724c18f7ee6cdbd8fa5714
Behruz <behruzqalqonov@gmail.com>
2026-07-11 23:03:07 +0500

    salom

--- algo30day/9sildingwindow/3.py
+++ algo30day/9sildingwindow/3.py
@@ -5,8 +5,7 @@ def longest_unique(lst):
         if lst[R] not in lines:
             lines.add(lst[R])
             R += 1
-        else:
-            lines.remove(lst[R])
+
     return len(lines)
 
 print(longest_unique([1,2,3,3,3,3]))
